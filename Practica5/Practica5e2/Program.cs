99ccc24e0068cdb03c23d9df594a11e6a2a1fbbb
MasterSSOA <tiburon.grande77@gmail.com>
2020-09-20 01:21:30 -0400

    Ejercicio 2 Finalizado.

--- Practica5/Practica5e2/Program.cs
+++ Practica5/Practica5e2/Program.cs
@@ -1,4 +1,4 @@
-﻿//Práctica 5 Ejercicio 1.
+﻿//Práctica 5 Ejercicio 2.
 //Nota : Programa capaz registrar 4 notas a traves de un arreglo.
 //
 // 
@@ -6,28 +6,20 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Practica5e2
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            List<double> Notas = new List<double>
+        List<double> Notas = new List<double>
             {
                 90, 88, 92, 79
             };
 
-            for (int i = 0; i < Notas.Count; i++)
-            {
-                Console.WriteLine("La Nota {0} es : {1}\n", i + 1, Notas[i]);
-            }
-            Console.ReadKey();
+        for (int i = 0; i < Notas.Count; i++)
+        {
+            Console.WriteLine("La Nota {0} es : {1}\n", i + 1, Notas[i]);
         }
+        Console.ReadKey();
     }
-
-    
 }
