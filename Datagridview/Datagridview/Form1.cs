22141b939d15a10461ad0a86758cac7340cc8f0d
MasterSSOA <tiburon.grande77@gmail.com>
2020-09-20 22:11:43 -0400

    Practica 5 Finalizada.

---
+++ Datagridview/Datagridview/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datagridview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Person> people = new List<Person>()
+            { 
+                new Person{ Name = "Hola",},
+                 new Person{ Name = "Hola2",}
+            };
+
+            var list = new BindingList<Person>(people);
+            dataGridView1.DataSource = list;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            dataGridView1.Rows[3].Cells[1].Value = textBox1.Text;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        class Person 
+        { 
+            public string Name { get; set; }
+            public string LastName { get; set; }
+        }
+
+    }
+}
---
+++ Practica5/Practica5e5/Program.cs
@@ -0,0 +1,60 @@
+﻿//Práctica 5 Ejercicio 5.
+//Nota : Programa capaz de calcular el producto vectorial de dos
+//       vectores.
+//
+// 
+//                                         © Anthony Reyes Duran
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica5e5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Columna;
+            int Producto1;
+            int Producto2;
+            int TotalVector1 = 0;
+            int TotalVector2 = 0;
+            int TotalVector3 = 0;
+
+            //Arreglo.
+            int[,] Matriz = { { 2, 4, 6}, { 1, 3, 5} };
+
+            //Lógica.
+            Columna = 0;
+
+            if (Columna < 3)
+            {
+                if (Matriz[0, Columna] == Matriz[0, 0])
+                {
+                    Producto1 = Matriz[0, 1] * Matriz[1, 2];
+                    Producto2 = Matriz[1, 1] * Matriz[0, 2];
+                    TotalVector1 = Producto1 + Producto2;
+                    Columna++;
+                    if (Matriz[0, Columna] == Matriz[0, 1])
+                    {
+                        Producto1 = Matriz[0, 0] * Matriz[1, 2];
+                        Producto2 = Matriz[1, 0] * Matriz[0, 2];
+                        TotalVector2 = Producto1 + Producto2;
+                        Columna++;
+                        if (Matriz[0, Columna] == Matriz[0, 2])
+                        {
+                            Producto1 = Matriz[0, 0] * Matriz[1, 1];
+                            Producto2 = Matriz[1, 0] * Matriz[0, 1];
+                            TotalVector3 = Producto1 + Producto2;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("El Producto Vectorial Es : <{0}, {1}, {2}>", TotalVector1, TotalVector2, TotalVector3);
+            Console.ReadKey();
+        }
+    }
+}
