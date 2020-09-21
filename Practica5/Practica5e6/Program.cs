c8b21f7f7e18a30ad79b2d92295c0ff90e23b5eb
MasterSSOA <tiburon.grande77@gmail.com>
2020-09-20 22:29:38 -0400

    Practica 6 Finalizada.

---
+++ Practica5/Practica5e6/Program.cs
@@ -0,0 +1,38 @@
+﻿//Práctica 5 Ejercicio 6.
+//Nota : Programa capaz de calcular el producto escalar de dos
+//       vectores.
+//
+// 
+//                                         © Anthony Reyes Duran
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica5e6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int subTotal;
+            int Total = 0;
+
+            //Arreglo.
+            int[] VectorA = { 2, 4, 6 };
+            int[] VectorB = { 1, 3, 5 };
+
+            //Lógica.
+            for (int i = 0; i < VectorA.Length; i++)
+            {
+                subTotal = VectorA[i] * VectorB[i];
+                Total += subTotal;
+            }
+
+            Console.WriteLine("El Producto Escalar Es : {0}", Total);
+            Console.ReadKey();
+        }
+    }
+}
